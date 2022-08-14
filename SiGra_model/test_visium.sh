afa2f43c271d08031e60c516a9e342f15258974b
dao2 <tangziyang666@gmail.com>
2022-08-14 15:41:02 -0400

    add bash

--- SiGra_model/test_visium.sh
+++ SiGra_model/test_visium.sh
@@ -1,13 +1,13 @@
 # /usr/bin/bash
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151507 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151508 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151509 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151510 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151669 --ncluster 5 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151670 --ncluster 5 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151671 --ncluster 5 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151672 --ncluster 5 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151673 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151674 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151675 --ncluster 7 --dataset 10x --cluster_method mclust
-python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151676 --ncluster 7 --dataset 10x --cluster_method mclust
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151507 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151508 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151509 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151510 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151669 --ncluster 5 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151670 --ncluster 5 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151671 --ncluster 5 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151672 --ncluster 5 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151673 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151674 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151675 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
+python3 -W ignore train.py --test_only 1 --save_path ../checkpoint/10x_final/ --id 151676 --ncluster 7 --dataset 10x --cluster_method mclust --root ../dataset/DLPFC
