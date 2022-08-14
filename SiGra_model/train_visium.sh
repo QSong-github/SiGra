4142437a0817ec88ce4aec0f321c0d920ddf3ea7
Qianqian Song <wasqqdyx@gmail.com>
2022-08-13 21:30:15 -0400

    Add files via upload

---
+++ SiGra_model/train_visium.sh
@@ -0,0 +1,19 @@
+lr=1e-3
+epoch=600
+seed=1234
+repeat=1
+sp='../checkpoint/transformer_ss'
+
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151676 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151675 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151674 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151673 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151672 --seed $seed --repeat $repeat --ncluster 5 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151671 --seed $seed --repeat $repeat --ncluster 5 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151670 --seed $seed --repeat $repeat --ncluster 5 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151669 --seed $seed --repeat $repeat --ncluster 5 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151510 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151509 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151508 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+python3 show_img_transformer.py --lr $lr --epochs $epoch --id 151507 --seed $seed --repeat $repeat --ncluster 7 --save_path $sp
+
