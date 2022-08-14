6d286dbd59efe7f54ae1ca0d20986efc011d2ce5
dao2 <tangziyang666@gmail.com>
2022-08-14 07:32:42 -0400

    add scripts

---
+++ SiGra_model/processing.py
@@ -0,0 +1,17 @@
+import argpase
+from processing_merscope import processing_mscope
+from processing_nanostring import processing_nano
+from processing_visium import processing_10x
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='nanostring', help='should be nanostring, merscope or 10x')
+opt = parser.parse_args()
+
+if opt.dataset == 'nanostring':
+	processing_nano()
+elif opt.dataset == 'merscope':
+	processing_mscope()
+elif opt.dataset == '10x':
+	processing_10x()
+
+
