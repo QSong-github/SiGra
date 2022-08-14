b8371cc7cfd7cec6b3418994a7bb7faf51d687fa
dao2 <tangziyang666@gmail.com>
2022-08-14 15:31:09 -0400

    add setup

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 import setuptools
+import io
+import os
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -26,5 +45,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
