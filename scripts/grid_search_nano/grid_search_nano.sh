9d8ad2caeeaefafd32033cf330ad854cd26b3157
tonyyang1995 <tangziyang666@gmail.com>
2023-02-05 22:02:04 +0000

    update scripts

--- scripts/grid_search_nano/grid_search_nano.sh
+++ scripts/grid_search_nano/grid_search_nano.sh
@@ -4,7 +4,7 @@ for g_weight in $(seq 0.1 0.2 1.0)
 do
     for i_weight in $(seq 0.1 0.2 1.0)
     do 
-        python3 train_nano_single_slice_grid_search.py --test_only 1 --g_weight $g_weight --i_weight $i_weight --num_fov 20 --save_path ../checkpoint/nano_grid/ --root ../dataset/nanostring --id lung9-1 --root ../dataset/nanostring 
+        python3 grid_search_nano.py --test_only 1 --g_weight $g_weight --i_weight $i_weight --num_fov 20 --save_path ../checkpoint/nano_grid/ --root ../dataset/nanostring --id lung9-1 --root ../dataset/nanostring 
     done
 done
 
